45e6847998657fd00905845059672279037075bb
jjjjonathan <hornathanjon@gmail.com>
2021-11-09 22:37:32 -0800

    Add chat app UI from tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Placeholder function for handling changes to our chat bar
+  const handleChange = () => {};
+
+  // Placeholder function for handling the form submission
+  const handleSubmit = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="messages">
+        <div className="messages-scroller">
+          {/* messages will be loaded here */}
+        </div>
+      </div>
+      <div className="chat-bar">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="messageBody"
+            placeholder="Type your message here"
+            onChange={handleChange}
+            value={''}
+          />
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
