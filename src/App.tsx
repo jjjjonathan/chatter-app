af5ae90b36fc6640c4725daee8e90d6908241360
jjjjonathan <hornathanjon@gmail.com>
2021-11-09 23:04:09 -0800

    Add MUI input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const App = () => {
   // Placeholder function for handling changes to our chat bar
@@ -17,12 +19,19 @@ const App = () => {
       </div>
       <div className="chat-bar">
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
+          <TextField
+            variant="outlined"
             name="messageBody"
             placeholder="Type your message here"
             onChange={handleChange}
             value={''}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
           />
         </form>
       </div>
